7d6cca073b71198cebdae5303fd4038a0b02e76d
Iryna Popova <popovaira969@gmail.com>
2023-09-22 21:35:00 +0300

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,65 +1,10 @@
 import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <div className="App">
-      {" "}
-      <div className="container">
-        <form className="form">
-          <p className="form__title">Залиште свої дані, ми вам передзвонимо</p>
-          <ul>
-            <li className="form__item">
-              <label htmlFor="name">Ім'я</label>
-              <div className="form__wrap">
-                <input
-                  className="form__input"
-                  type="text"
-                  name="name"
-                  id="name"
-                />
-                <svg className="form__icon">
-                  <use href="#icon-person-form"></use>
-                </svg>
-              </div>
-            </li>
-            <li className="form__item">
-              <label htmlFor="tel">Телефон</label>
-              <div className="form__wrap">
-                <svg className="form__icon">
-                  <use href="icons#icon-phone-form"></use>
-                </svg>
-                <input className="form__input" type="tel" id="tel" name="tel" />
-              </div>
-            </li>
-            <li className="form__item">
-              <label htmlFor="email">Пошта</label>
-              <div className="form__wrap">
-                <svg className="form__icon">
-                  <use href="./icons.svg#icon-email-form"></use>
-                </svg>
-                <input
-                  className="form__input"
-                  type="email"
-                  id="email"
-                  name="email"
-                />
-              </div>
-            </li>
-            <li className="form__item">
-              <label htmlFor="feedback">Коментар</label>
-              <textarea
-                className="form__textarea"
-                name="feedback"
-                id="feedback"
-                placeholder="Введіть текст"
-              ></textarea>
-            </li>
-          </ul>
-          <button type="submit" className="button">
-            Відправити
-          </button>
-        </form>
-      </div>
+    <Form/>
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,74 @@
+import css from "./Form.module.css";
+import PersonIcon from "../Icons/PepsonIcon";
+import PhoneIcon from "../Icons/PhoneIcon";
+import EmailIcon from "../Icons/EmailIcon";
+
+const Form = () => {
+  return (
+    <div className={css.container}>
+      <form className={css.form}>
+        <p className={css.form__title}>
+          Залиште свої дані, ми вам передзвонимо
+        </p>
+        <ul>
+          <li className={css.form__item}>
+            <label htmlFor="name">Ім'я</label>
+            <div className={css.form__wrap}>
+              <input
+                className={css.form__input}
+                type="text"
+                name="name"
+                id="name"
+              />
+              <div className={css.form__icon}>
+                <PersonIcon />
+              </div>
+            </div>
+          </li>
+          <li className={css.form__item}>
+            <label htmlFor="tel">Телефон</label>
+            <div className={css.form__wrap}>
+              <div className={css.form__icon}>
+                <PhoneIcon />
+              </div>
+              <input
+                className={css.form__input}
+                type="tel"
+                id="tel"
+                name="tel"
+              />
+            </div>
+          </li>
+          <li className={css.form__item}>
+            <label htmlFor="email">Пошта</label>
+            <div className={css.form__wrap}>
+              <div className={css.form__icon}>
+                <EmailIcon />
+              </div>
+              <input
+                className={css.form__input}
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+          </li>
+          <li className={css.form__item}>
+            <label htmlFor="feedback">Коментар</label>
+            <textarea
+              className={css.form__textarea}
+              name="feedback"
+              id="feedback"
+              placeholder="Введіть текст"
+            ></textarea>
+          </li>
+        </ul>
+        <button type="submit" className={css.button}>
+          Відправити
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
