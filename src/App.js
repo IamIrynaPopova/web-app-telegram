a52a696ff837dc8d006cb6f42755cb42845c656a
Iryna Popova <popovaira969@gmail.com>
2023-09-27 00:19:09 +0300

    ADDED popup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./components/Form/Form";
+import Popup from "./components/Popup/Popup";
 
 function App() {
   return (
     <div className="App">
-    <Form/>
+      <Form />
+      <Popup/>
     </div>
   );
 }
---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,26 @@
+import css from "./Popup.module.css";
+import PopupIcon from "../Icons/PopupIcon";
+
+const Popup = () => {
+  return (
+    <div className={css.popup}>
+      <div className={css.popup__icon}>
+        <PopupIcon />
+      </div>
+      <div className={css.popup__conteiner}>
+        <div className={css.popup__title__wrapper}>
+          <p className={css.popup__title}>Вітаю!</p>
+        </div>
+        <div className={css.popup__text__wrapper}>
+          <p className={css.popup__text}>
+            Ваше повідомлення успішно відправлено.<br></br>Ми дамо відповідь
+            якнайшвидше
+          </p>
+        </div>
+        <button className={css.popup__button}>ОК</button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
